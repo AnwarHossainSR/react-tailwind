2854f007fc78ba286cc822112dd3473797ebbfec
Anwar Hossain <anwar.sarker@bjitgroup.com>
2023-02-14 12:54:12 +0600

    completed

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,14 @@
+import Sidebar from '../../components/SIdebar';
+
 const index = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex align-center gap-2 min-h-screen py-2">
+      <Sidebar />
+      <main>
+        <h1>Home</h1>
+      </main>
+    </div>
+  );
 };
 
 export default index;
